468630f2d383aa82c18c8be14d87dca7f9fd0a9c
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-31 20:06:25 -0200

    Quote Fragment update.
    Added a code to fetch the elements from the XML layout file. Added a static method to handle the creation of the fragment, passing both quote and auhor as parameters. Added the code to set the values passed by intent into the UI.

--- app/src/main/java/devarthur/com/lifetips/QuoteFragment.java
+++ app/src/main/java/devarthur/com/lifetips/QuoteFragment.java
@@ -3,16 +3,16 @@ package devarthur.com.lifetips;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment; //Suport older versions of fragment in older devices
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class QuoteFragment extends Fragment {
+
 
 
     public QuoteFragment() {
@@ -25,7 +25,30 @@ public class QuoteFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View quoteView = inflater.inflate(R.layout.fragment_quote, container, false);
+
+        TextView quoteText = quoteView.findViewById(R.id.quoteText);
+        TextView authorName = quoteView.findViewById(R.id.author);
+        CardView cardview = quoteView.findViewById(R.id.cardview);
+
+        String quote = getArguments().getString("quote");
+        String author = getArguments().getString("author");
+
+        quoteText.setText(quote);
+        authorName.setText("-" +  author);
+
         return quoteView;
+    }
+
+    public static final QuoteFragment newInstance(String quote, String author){
+        QuoteFragment fragment = new QuoteFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("quote", quote);
+        bundle.putString("author", author);
+        fragment.setArguments(bundle);
+
+
+        return fragment;
+
     }
 
 }
