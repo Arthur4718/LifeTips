4fee7cbb577897bde795421c8592c7dfdbd5ac6a
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-31 15:59:00 -0200

    Quote Model classs

---
+++ app/src/main/java/devarthur/com/lifetips/model/QuoteModel.java
@@ -0,0 +1,24 @@
+package devarthur.com.lifetips.model;
+
+//Defines de quote data model
+
+public class QuoteModel {
+    private String quote;
+    private String author;
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
