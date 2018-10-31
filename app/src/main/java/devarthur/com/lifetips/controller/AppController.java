11f5232500b4ccaf71cbc73808edcee96efd5508
Unknown <arthur.gomes_4718@hotmail.com>
2018-10-31 16:45:42 -0200

    App controller generic methods.

--- app/src/main/java/devarthur/com/lifetips/controller/AppController.java
+++ app/src/main/java/devarthur/com/lifetips/controller/AppController.java
@@ -33,7 +33,17 @@ public class AppController  extends Application {
     public <T> void addToRequestQueue(Request <T> req, String tag){
 
         req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-        getmRequestQueue().add(req); 
+        getmRequestQueue().add(req);
+
+    }
+    public <T> void addToRequestQueue(Request <T> req){
+        req.setTag(TAG);
+        getmRequestQueue().add(req);
+    }
+    public void cancelPendingRequests(Object tag){
+        if(mRequestQueue != null){
+            mRequestQueue.cancelAll(tag);
+        }
 
     }
 }
